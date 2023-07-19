3ca9283f3bf7665e5b2552f24b98ed6a65fbc028
fendishatech <liyukibir@gmail.com>
2023-07-19 07:29:44 -0700

    fronten: pages, routes, dashboard layout setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Products from "./pages/products/";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
 function App() {
   return (
-    <h1 className="py-6 text-center text-3xl text-green-400 font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/products/index.jsx
@@ -0,0 +1,11 @@
+import Dashboard from "../dashboard/index";
+
+const Products = () => {
+  return (
+    <Dashboard>
+      <h1 className="text-center text-4xl text-blue-400">Products</h1>
+    </Dashboard>
+  );
+};
+
+export default Products;
